c09800bc6cfe094589df8e1e9a6ace9f2f2848f1
성승연 <seungyeon.sung@h9works.com>
2024-02-06 17:19:59 +0900

    feat : post detail navigation 기능 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -12,9 +12,15 @@ type Props = {
 };
 
 export default async function PostPage({ params: { slug } }: Props) {
-  const posts = await getPosts();
+  const beforeSortPosts = await getPosts();
   const data = await getPostData(slug);
+  const posts = beforeSortPosts
+    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+    .reverse();
+  const index = posts.findIndex((obj) => obj.path == slug);
   const post = posts.filter((val) => val.path === slug)[0];
+  const prevPost = index === 0 ? posts[posts.length - 1] : posts[index - 1];
+  const nextPost = index === posts.length - 1 ? posts[0] : posts[index + 1];
 
   if (!data) {
     redirect('/posts');
@@ -37,7 +43,7 @@ export default async function PostPage({ params: { slug } }: Props) {
             <FaCalendar className='inline-block mr-2 mb-0.5' />
             {post.date}
           </div>
-          <h1 className='text-4xl  font-bold'>{post.title}</h1>
+          <h1 className='text-4xl font-bold'>{post.title}</h1>
           <p className='pt-1'>{post.description}</p>
           <span className='inline-block w-32 h-1 bg-lime-300'></span>
         </div>
@@ -47,24 +53,50 @@ export default async function PostPage({ params: { slug } }: Props) {
 
         {/* navigation */}
         <div className='flex h-60 mb-10'>
-          <div className='flex-1 bg-white rounded-bl-lg'>
-            <Link href='' className='relative inline-block w-full h-full'>
+          <div className='flex-1 bg-white rounded-bl-lg overflow-hidden'>
+            <Link
+              href={`/posts/${prevPost.path}`}
+              className='relative inline-block w-full h-full bg-black'
+            >
+              <div className='absolute inline-block w-full h-full z-0'>
+                <Image
+                  src={`/images/posts/${prevPost.path}.png`}
+                  alt=''
+                  layout='fill'
+                />
+              </div>
+              <div className='absolute inline-block w-full h-full z-1 bg-black opacity-60'></div>
               <FaArrowLeft
-                color='black'
-                className='absolute left-8 top-1/2 -translate-y-2/4 w-8 h-8'
+                color='white'
+                className='absolute left-8 inset-y-1/2 -translate-y-1/2 w-8 h-8 z-2'
               />
-              <div></div>
-              <div></div>
+              <div className='inline-block absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center z-3'>
+                <div className='text-2xl font-bold'>{prevPost.title}</div>
+                <div className='mt-1'>{prevPost.description}</div>
+              </div>
             </Link>
           </div>
-          <div className='flex-1 bg-white rounded-br-lg'>
-            <Link href='' className='relative inline-block w-full h-full'>
+          <div className='flex-1 bg-white rounded-br-lg overflow-hidden'>
+            <Link
+              href={`/posts/${nextPost.path}`}
+              className='relative inline-block w-full h-full bg-black'
+            >
+              <div className='absolute inline-block w-full h-full z-0'>
+                <Image
+                  src={`/images/posts/${nextPost.path}.png`}
+                  alt=''
+                  layout='fill'
+                />
+              </div>
+              <div className='absolute inline-block w-full h-full z-1 bg-black opacity-60'></div>
               <FaArrowRight
-                color='black'
-                className='absolute right-8 top-1/2 -translate-y-2/4 w-8 h-8'
+                color='white'
+                className='absolute right-8 inset-y-1/2 -translate-y-1/2 w-8 h-8 z-1'
               />
-              <div></div>
-              <div></div>
+              <div className='inline-block absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center z-2'>
+                <div className='text-2xl font-bold'>{nextPost.title}</div>
+                <div className='mt-1'>{nextPost.description}</div>
+              </div>
             </Link>
           </div>
         </div>
